901f8187f4b41ef0424a80823795f5b74a24e85c
nabil.lahssini <nabil.lahssini@student.ehb.be>
2021-08-26 01:46:19 +0200

    Structural changes

--- setup.py
+++ setup.py
@@ -7,14 +7,13 @@ HERE = pathlib.Path(__file__).parent
 # The text of the README file
 README = (HERE / "README.md").read_text()
 
-# This call to setup() does all the work
 setup(
     name="ascii_art",
     version="1.0.0",
     description="The ASCII art tool is a simple library to generate ASCII art from regular images.",
     long_description=README,
     long_description_content_type="text/markdown",
-    url="https://github.com/Nabil-Lahssini/ascii_art",
+    url="https://github.com/Nabil-Lahssini/ASCII_art",
     author="Nabil Lahssini",
     author_email="NabilLahssini@gmail.com",
     license="MIT",
