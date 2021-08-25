2c76a0448b6a41866d551cee731b3c4c68c39378
nabil.lahssini <nabil.lahssini@student.ehb.be>
2021-08-26 01:50:43 +0200

    V1.0.0 release

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
 setup(
-    name="ascii_art",
+    name="ASCII_art_tool",
     version="1.0.0",
     description="The ASCII art tool is a simple library to generate ASCII art from regular images.",
     long_description=README,
     long_description_content_type="text/markdown",
-    url="https://github.com/Nabil-Lahssini/ASCII_art",
+    url="https://github.com/Nabil-Lahssini/ASCII_art_tool",
     author="Nabil Lahssini",
     author_email="NabilLahssini@gmail.com",
     license="MIT",
